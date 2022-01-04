3412e55d73bc27b1a09fcc3a7a0287e9d2247ba3
Eugene <evgeniykip94@gail.com>
2022-01-04 17:35:41 +0200

    filterDone

--- src/js/component/filter.js
+++ src/js/component/filter.js
@@ -1,20 +1,23 @@
 import { createComment } from './comment';
 
 function filterComment(createArray) {
-  console.log(createArray[0].created_at);
-
-  console.log(createArray[0].created_at.substring(0, 10));
-  // console.log(createArray.sort((a, b) => a.created_at - b.created_at));
-  // console.log(createArray.sort((a, b) => b.created_at - a.created_at));
-
   const filter = document.querySelectorAll('.filter');
   filter.forEach((elem) => {
     elem.addEventListener('click', (e) => {
       if (e.target.innerText === 'Most Liked') {
         createComment(createArray.sort((a, b) => b.like - a.like));
       } else if (e.target.innerText === 'Newest') {
-        console.log('Newest');
-        createComment(createArray.sort((a, b) => a.created_at - b.created_at));
+        createComment(
+          createArray.sort(
+            (b, a) => new Date(a.created_at) - new Date(b.created_at)
+          )
+        );
+      } else if (e.target.innerText === 'Oldest') {
+        createComment(
+          createArray.sort(
+            (b, a) => new Date(b.created_at) - new Date(a.created_at)
+          )
+        );
       }
     });
   });
